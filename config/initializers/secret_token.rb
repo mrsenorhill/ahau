abc00e93e67ece9cf7c7774aeeac9178643fe479
Your Name <gregoryehill@gmail.com>
2013-02-02 16:02:32 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ahau::Application.config.secret_token = '88696fa0da36cc9e35f0e5a935fd558414db5e1dc1dcc9ee628655b9b7dae96bc2cc9667bd5dd14a88e9f436c93fab36d0a186618946a0fbbb4228ed5abbb8f0'
